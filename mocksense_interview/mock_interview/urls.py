f8300c91b52eab90539ec18dbf989a2aefdbe8cc
SANMUGAM2004 <hmsanmugam@gmail.com>
2025-02-18 21:44:30 +0530

    Updated

--- mocksense_interview/mock_interview/urls.py
+++ mocksense_interview/mock_interview/urls.py
@@ -7,12 +7,14 @@ from . import views
     # path("start/", views.start, name="start_interview"),  # Start interview
     # path("result/", views.result, name="mock_interview_result"),  # Result page
 urlpatterns = [
+    path("", views.index, name="mock_interview_index"),
     path('start/', views.start_interview, name='start_interview'),
     path('quiz/', views.quiz_view, name='mock_interview_quiz'),
     path('video_feed/', views.video_feed, name='mock_interview_video_feed'),
+     path("save_answer/", views.save_answer, name="save_answer"),
     path('listen/', views.listen_answer, name='mock_interview_listen'),
-    path('ask_question/<int:question_index>/', views.ask_questions, name='ask_question'),
-    path('ask_question/', views.ask_questions, name='ask_question'),
+    # path('ask_question/<int:question_index>/', views.ask_questions, name='ask_question'),
+    # path('ask_question/', views.ask_questions, name='ask_question'),
     path('result/', views.mock_interview_result, name='mock_interview_result'),  # Add this line
 ]
 
