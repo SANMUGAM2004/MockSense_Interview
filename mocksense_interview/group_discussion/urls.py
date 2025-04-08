603d1aeabf6a209e1f9ac1eedda23dd743377948
SANMUGAM2004 <hmsanmugam@gmail.com>
2025-04-08 18:10:52 +0530

    adding Group disscussion modules

--- mocksense_interview/group_discussion/urls.py
+++ mocksense_interview/group_discussion/urls.py
@@ -7,19 +7,18 @@
 #     path('detect_emotion/', detect_emotion, name='detect_emotion'),
 # ]
 from django.urls import path
-from .views import discussion_view, detect_emotion, group_discussion
+from .views import discussion_view, detect_emotion, group_discussion,start_grp_discussion_view,group_discussion_view,improve_sentences_view,save_user_gd_response
 
 urlpatterns = [
     path("", discussion_view, name="discussion"),
+    path("start", start_grp_discussion_view, name="start_group_discussion"),
     path("detect_emotion/", detect_emotion, name="detect_emotion"),
+    path("disscussion", group_discussion_view, name="group_disscussion_view"),
+    path('save_response/', save_user_gd_response, name='save_user_gd_response'),
+    # path('save_response/', save_response, name='save_response'),
+    # path('improve/', improve_sentences_view, name='improve_sentences'),
+    path('result/', improve_sentences_view, name='gd_result'),
+
     # path("group_discussion/", group_discussion, name="group_discussion"),
     path("gd/", group_discussion, name="group_discussion"),
 ]
-# from django.urls import path
-# from .views import detect_emotion, discussion_view, group_discussion
-
-# urlpatterns = [
-#     path("detect_emotion/", detect_emotion, name="detect_emotion"),
-#     path("discussion/", discussion_view, name="discussion_view"),
-#     path("gd/api/", group_discussion, name="group_discussion"),
-# ]
